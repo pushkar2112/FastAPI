d08c111d0a21c03778e8bc7ad8372d208be02bab
Pushkar Verma <pushkar7397@gmail.com>
2022-07-05 11:16:18 +0530

    first commit

---
+++ myapi.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI() # Create a FastAPI instance
+
+# Endpoint methods
+# GET - GET INFO
+# POST - CREATE SOMETHING NEW
+# PUT - UPDATE
+# DELETE - DELETE SOMETHING
+
+
