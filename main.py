cd49bc6b8711342b43cedeb1cc37a2a3a4ff3a91
Pushkar Verma <pushkar7397@gmail.com>
2022-07-06 15:20:32 +0530

    Get post by ID and response codes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -29,7 +29,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0,10000000)
@@ -37,8 +37,14 @@ def create_posts(post: Post):
     return {"data": post_dict}
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(id)
+
+    if not post:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": "Post Not Found!!"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Post Not Found!!")
     return {"post_details": post} 
 
 
