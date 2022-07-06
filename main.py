5ac1a813494dadd3e4ff08a5de702e9da2c5dcb1
Pushkar Verma <pushkar7397@gmail.com>
2022-07-06 14:30:22 +0530

    Add post model and improve post route

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from turtle import title
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -12,6 +19,8 @@ def get_posts():
     return {"data": "This is your post!"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
+
