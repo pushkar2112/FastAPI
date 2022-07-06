5b6f8b6b9c12b58e6d74064d6b74973a3e498810
Pushkar Verma <pushkar7397@gmail.com>
2022-07-06 14:41:54 +0530

    Add sample posts

--- main.py
+++ main.py
@@ -10,15 +10,20 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [
+    {"title": "title of post1", "content": "content of post 1", "id": 1}, 
+    {"title": "favourite foods", "content": "I like pizza!!", "id": 2}
+    ]
+
 @app.get("/")
 def root():
     return {"message": "Welcome to my API!"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your post!"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
     print(post)
     print(post.dict())
