41a3ef477c78ccf8bf38c6cd2c832db4a56e9a97
Pushkar Verma <pushkar7397@gmail.com>
2022-07-06 22:58:10 +0530

    Update posts

--- main.py
+++ main.py
@@ -58,3 +58,17 @@ def delete_post(id: int):
     del my_posts[post_index]
 # We do not return a message!
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    
+    try:
+        post_index = my_posts.index(find_post(id))
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post does not exists!!")
+
+    post_dict = post.dict() 
+    post_dict["id"] = id
+    my_posts[post_index] = post_dict 
+
+    return {"data": post_dict}
