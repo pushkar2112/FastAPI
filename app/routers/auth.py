2a74fc3ef43c9c9fa37bc92b73c68e1dae87733a
Pushkar Verma <pushkar7397@gmail.com>
2022-07-11 01:05:21 +0530

    Refactor using Oaut2

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
 
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+# Returns username and password OauthForm
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
