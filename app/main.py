35796d84123567c530809f1e12cabeaf09218476
Pushkar Verma <pushkar7397@gmail.com>
2022-07-11 00:44:25 +0530

    Add authentication support

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from random import randrange
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -40,6 +40,7 @@ def find_post(id):
 # We make router to seperate out the code in pur file
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
